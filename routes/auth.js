b46eab5b19c45c398bd7d5f05a0bfc49c46460c9
Caio <caiocesar333@outlook.com>
2022-12-02 00:32:07 -0300

    fix: resolvendo alguns bugs e fazendo a autentificação para admin

--- routes/auth.js
+++ routes/auth.js
@@ -5,44 +5,64 @@ const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password,
-            "compasso")
-            .toString()
-    });
-    try {
-        const savedUser = await newUser.save();
-        res.status(200).json(savedUser);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-})
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      "compasso"
+    ).toString(),
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //LOGIN
 
-router.post("/login", async (req, res) => {
-    try {
-        const user = await User.findOne({
-            username:req.body.username
-        });
-        !user && res.status(401).json("Wrong User name");
+router.post('/login', async (req, res) => {
+    try{
+        const user = await User.findOne(
+            {
+                userName: req.body.user_name
+            }
+        );
 
-        const hashPassword = CryptoJS.AES.decrypt(user.password, "compasso");
-        const Originalpassword = hashPassword.toString(CryptoJS.enc.Utf8);
-        Originalpassword !== req.body.password && res.status(401).json("Wrong Password");
+        !user && res.status(401).json("Wrong User Name");
 
-        const acessToken = jwt.sign({
-            id:user._id, isAdmin: user.isAdmin
-        }, "Compasso", {expiresIn:"3d"})
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            "compasso"
+        );
 
-        const {password, ...others} = user._doc;
 
-        res.status(200).json({...others, acessToken})
-    } catch (err) {
-        res.status(500).json(err)
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        const inputPassword = req.body.password;
+        
+        originalPassword != inputPassword && 
+            res.status(401).json("Wrong Password");
+
+        const accessToken = jwt.sign(
+        {
+            id: user._id,
+            isAdmin: user.isAdmin,
+        },
+        "compasso",
+            {expiresIn:"3d"}
+        );
+  
+        const { password, ...others } = user._doc;  
+        res.status(200).json({...others, accessToken});
+
+    }catch(err){
+        res.status(500).json(err);
     }
-})
+
+});
 
 module.exports = router;
