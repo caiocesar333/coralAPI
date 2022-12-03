3031448fc1cc3926db51cbcf794585df8880f1bb
Caio <caiocesar333@outlook.com>
2022-12-03 16:43:07 -0300

    Adicionando a rota stripe

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+// const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
 dotenv.config();
 
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/user");
+mongoose
+  .connect("mongodb+srv://caiocesar333:mega12345678@cluster0.jut0lkc.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
-mongoose.connect(
-    "mongodb+srv://caiocesar333:mega12345678@cluster0.jut0lkc.mongodb.net/?retryWrites=true&w=majority"
-).then(()=>console.log("sucesso"))
-.catch((err)=>console.log(err));
-
+app.use(cors());
 app.use(express.json());
-app.use("/coral/auth", authRoute);
-app.use("/coral/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+// app.use("/api/checkout", stripeRoute);
 
-app.listen(5000, () => {
-
-})
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
